06a2204aad0372fb36f2302c17c981ddd51df337
Meletis Nikolaidis <melnikolaidis@hotmail.com>
2018-05-09 16:36:46 +0300

    Implementing many to many relationship queries

--- application/search.php
+++ application/search.php
@@ -13,26 +13,32 @@ mysqli_set_charset($mysqli_connection, "utf8");
 //Getting the search query
 if(isset($_POST['search'])) {
 
-    //Fetching results based on the search query
-    $posts = $mysqli->select("SELECT *  FROM posts
+    //Fetching results based on the search query looking up for posts per category
+    $posts_per_category = $mysqli->select("SELECT *  FROM posts
                                         INNER JOIN categories ON posts.category_id = categories.id
                                         WHERE categories.category_name LIKE '%" . $_POST['search'] . "%'")->get();
 
+    //Fetching results based on the search query looking up for posts per tag
+    $posts_per_tag = $mysqli->select("SELECT posts.post_id, posts.post_title FROM posts
+                                        LEFT JOIN post_tags ON post_tags.post_id = posts.post_id
+                                        LEFT JOIN tags ON tags.tag_id = post_tags.tag_id")->get();
+
+    var_dump($posts_per_tag);
 }
 
 ?>
 
-    <?php if(isset($posts)) : ?>
+    <?php if(isset($posts_per_category)) : ?>
 
-        <?php if($posts) : ?>
+        <?php if($posts_per_category) : ?>
 
-            <?php foreach($posts as $post) : ?>
+            <?php foreach($posts_per_category as $post_per_category) : ?>
 
             <li class="list-group-item">
                 <div class="media w-100">
                         <div class="media-body">
-                            <strong id="search-title"><?php echo $post->post_title; ?></strong>
-                            <p id="search-category">@ <?php echo $post->category_name; ?></p>
+                            <strong id="search-title"><?php echo $post_per_category->post_title; ?></strong>
+                            <p id="search-category">@ <?php echo $post_per_category->category_name; ?></p>
                         <hr>
                     </div>
                 </div>
---
+++ custom-cms.sql
@@ -0,0 +1,119 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 09, 2018 at 01:35 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `custom-cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(30) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(125) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category_name`) VALUES
+(1, 'πολιτικά'),
+(2, 'νέα');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `post_id` int(30) NOT NULL AUTO_INCREMENT,
+  `post_title` varchar(100) NOT NULL,
+  `post_content` text NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`post_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`post_id`, `post_title`, `post_content`, `category_id`, `created_date`) VALUES
+(1, 'Εκτίμηση Ψήφου', 'Λορεμ ιπσθμ δολορ σιτ αμετ, αδ vερεαρ vιδερερ ομιτταμ ηασ, vιδισσε βονορθμ εα vιμ, cθμ θτ διcιτ ειρμοδ. Μοδθσ μολεστιαε εοσ cθ. Πρι ατqθι δολορθμ φαcιλισ εα, vερο ιμπεδιτ φθισσετ vισ αδ. Περ αδ τριτανι δελενιτι, διcτασ δοcενδι εθμ τε. Qθασ cοντεντιονεσ δθο ετ, vιξ νατθμ cονσθλατθ ατ.', 1, '2018-05-07 05:48:50'),
+(2, 'Πρωτάθλημα Ελλάδος', 'Ιθσ αν ορνατθσ νεγλεγεντθρ, δενιqθε ρατιονιβθσ προ νε, μοδο μαιεστατισ σιγνιφερθμqθε προ εθ. Vελ qθοδ τεμπορ vολθτπατ εα. Vερεαρ οπορτεατ ιν περ, ηασ ετ ταμqθαμ δισσεντιασ. Θτ περπετθα περιcθλισ σιτ, εα cιβο ταντασ ιραcθνδια θσθ, εθ ιθσ αλιqθιπ φαβθλασ.', 2, '2018-05-07 05:48:50'),
+(3, 'Επίδομα Εργασίας', 'Εθ qθο ελιτ cηορο cονvενιρε, μελ νοβισ ιντελλεγατ αδ. Ετ vελ ελιτρ σολεατ, μεα ειθσ cηορο ει, περ ελιτ σαλε ηαρθμ εθ. Εθ ετιαμ φαcιλισ εστ. Εοσ νο αμετ αλιqθιπ νεcεσσιτατιβθσ.', 2, '2018-05-07 05:49:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_tags`
+--
+
+DROP TABLE IF EXISTS `post_tags`;
+CREATE TABLE IF NOT EXISTS `post_tags` (
+  `post_id` int(30) NOT NULL,
+  `tag_id` int(30) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `post_tags`
+--
+
+INSERT INTO `post_tags` (`post_id`, `tag_id`) VALUES
+(1, 1),
+(1, 4),
+(2, 2),
+(2, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+DROP TABLE IF EXISTS `tags`;
+CREATE TABLE IF NOT EXISTS `tags` (
+  `tag_id` int(30) NOT NULL AUTO_INCREMENT,
+  `tag_name` varchar(200) NOT NULL,
+  PRIMARY KEY (`tag_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`tag_id`, `tag_name`) VALUES
+(1, 'εκλογές'),
+(2, 'ποδόσφαιρο'),
+(3, 'μπάσκετ'),
+(4, 'περιφέρεια Αθηνών');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
